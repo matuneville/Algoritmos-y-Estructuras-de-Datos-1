4618fdf5c727adee5e4e2e10b08c13eb6d1f5078
matuneville <matineville@gmail.com>
2022-09-03 16:11:18 -0300

    change via clion

--- Labo/Resueltos/labo02/2-4.cpp
+++ Labo/Resueltos/labo02/2-4.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include <cmath>
+#include <math.h>
 using namespace std;
 
 int main () {
     int binaryNum;
     int decimalNum = 0;
-    int potencia = 0;
+    int i = 0;
     cin>> binaryNum;
 
     while (binaryNum > 0){
         int digit = binaryNum % 10;
         if (digit == 1) {
-            decimalNum += pow(2, potencia);
+            decimalNum += pow(2, i);
         }
-        potencia++;
+        i++;
         binaryNum /= 10; // esto le quita la ultima cifra al numero
     }
 
