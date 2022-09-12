bd7d3fc61afb98c655c93a34af137929e5713f9c
matuneville <matineville@gmail.com>
2022-09-12 15:39:13 -0300

    changes

---
+++ Labo/Resueltos/labo03/template-alumnos/src/vectores.cpp
@@ -0,0 +1,52 @@
+#include "vectores.h"
+
+// Función para probar en main.cpp si este módulo fue cargado correctamente
+void holaModuloVectores(){
+    cout << "El modulo vectores fue cargado correctamente" << endl;
+}
+
+
+//Ejercicio 1
+bool divide(vector<int> v, int a){
+    //Dados un vector v y un entero a, decide si a divide a todos los elementos de v.
+    int i = 0;
+    while (i < v.size()){
+        if (v[i] % a != 0)
+            return false;
+        i++;
+    }
+    return true;
+}
+/*
+//Ejercicio 2
+int mayor(vector<int> v){
+	// Dado un vector v, devuelve el valor maximo encontrado.
+    int i, j;
+    for (i = 0; i < v.size() - 1; i++){
+        for (j = 1; j < v.size() && v[i] >= v[i+j], j++) {
+            return v[i];
+        }
+    }
+}
+*/
+//Ejercicio
+vector<int> reverso(vector<int> v){
+	// Dado un vector v, devuelve el reverso.
+}
+
+//Ejercicio
+vector<int> rotar(vector<int> v, int k){
+	// Dado un vector v y un entero k, rotar k posiciones los elementos de v.
+	// [1,2,3,4,5,6] rotado 2, deberia dar [3,4,5,6,1,2].
+}
+
+//Ejercicio
+vector<int> factoresPrimos(int n){
+	//que dado un entero devuelve un vector con los factores primos del mismo
+}
+
+//Ejercicio
+void mostrarVector(vector<int> v){
+	//que dado un vector de enteros muestra por la salida estándar, el vector
+	// Ejemplo: si el vector es <1, 2, 5, 65> se debe mostrar en pantalla [1, 2, 5, 65]
+}
