d5a1465b3b962f2ba414076aebc843812c87e893
matuneville <matineville@gmail.com>
2022-12-07 15:05:00 -0300

    fin de cuatri

---
+++ Labo/Resueltos/labo11/bin-package/src/cmake-build-debug/CMakeFiles/bin-package.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin-package.exe"
+  "../bin-package.exe.manifest"
+  "../bin-package.pdb"
+  "../libbin-package.dll.a"
+  "CMakeFiles/bin-package.dir/main.cpp.obj"
+  "CMakeFiles/bin-package.dir/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin-package.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
